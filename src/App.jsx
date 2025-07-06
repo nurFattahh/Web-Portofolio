b0ab633361566772cf57dae4bb6bb4c9fb1448b6
nurFattahh <nurfattah36@gmail.com>
2025-07-06 19:07:58 +0700

    feat: page transition animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -9,21 +10,31 @@ import About from "./components/About/About";
 import Project from "./components/Project/Project";
 import Organization from "./components/Organization/Organization";
 import Experience from "./components/Experience/Experience";
-
 import ProjectDetail from "./components/Project/ProjectDetails";
+
+import MotionWrapper from "./components/Animations/MotionWrapper";
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<MotionWrapper><Home /></MotionWrapper>} />
+        <Route path="/about" element={<MotionWrapper><About /></MotionWrapper>} />
+        <Route path="/experience" element={<MotionWrapper><Experience /></MotionWrapper>} />
+        <Route path="/projects" element={<MotionWrapper><Project /></MotionWrapper>} />
+        <Route path="/organization" element={<MotionWrapper><Organization /></MotionWrapper>} />
+        <Route path="/projects/:id" element={<MotionWrapper><ProjectDetail /></MotionWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 const App = () => (
   <Router>
     <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/experience" element={<Experience />} />
-      <Route path="/projects" element={<Project />} />
-      <Route path="/organization" element={<Organization />} />
-      <Route path="/projects/:id" element={<ProjectDetail />} />
-    
-    </Routes>
+    <AnimatedRoutes />
     <Footer />
   </Router>
 );
